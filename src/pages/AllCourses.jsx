7bcee31ec13dad366a176b6b6fddd84fd339cf04
jaisonjeba <jaiprabakar5@gmail.com>
2024-08-28 13:53:39 +0530

    updated

--- src/pages/AllCourses.jsx
+++ src/pages/AllCourses.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import course from "../assets/All Course 1.png"
 
 const AllCourses = () => {
   return (
@@ -6,7 +7,7 @@ const AllCourses = () => {
       <h2>All Courses</h2>
       <p>This is the content for All Courses.</p>
       <div style={{display:'flex'}}>
-      <div style={{display:'flex',flexDirection:'column'}}><img src="src\assets\All Course.png" alt="" />
+      <div style={{display:'flex',flexDirection:'column'}}><img src={course} alt="" />
       <p>Python Objects</p>
       </div>
       <div style={{display:'flex',flexDirection:'column'}}><img src="src\assets\All Course 1.png" alt="" />
